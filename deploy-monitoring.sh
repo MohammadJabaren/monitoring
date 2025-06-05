7cbf7ad372f68c6264e35b0a34bc818ddcebff5e
MohammadJabaren <mohammad.eek.99@gmail.com>
2025-06-05 15:26:17 +0300

    implement prometheus and grafana

---
+++ deploy-monitoring.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+set -e
+
+#--------------------------- install Docker ---------------------------------------------
+if ! command -v docker &> /dev/null; then
+  echo "🚀 Docker not found. Installing..."
+  sudo apt-get update
+  sudo apt-get install -y ca-certificates curl
+  sudo install -m 0755 -d /etc/apt/keyrings
+  sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+  sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+  echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+    $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+    sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+  sudo apt-get update
+  sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+else
+  echo "✅ Docker is already installed."
+fi
+
+# 🔁 Re-check that Docker was installed correctly
+if ! command -v docker &> /dev/null; then
+  echo "❌ Docker installation failed or not found. Exiting."
+  exit 1
+fi
+
+docker network inspect monitoring-net >/dev/null 2>&1 || docker network create monitoring-net
+
+docker pull prom/prometheus
+docker pull grafana/grafana
+
+docker rm -f prometheus || true
+docker rm -f grafana || true
+
+docker run -d \
+  --name prometheus \
+  --network monitoring-net \
+  -p 9090:9090 \
+  -v $(pwd)/prometheus/prometheus.yml:/etc/prometheus/prometheus.yml \
+  prom/prometheus
+
+docker run -d \
+  --name grafana \
+  --network monitoring-net \
+  -p 3000:3000 \
+  grafana/grafana
